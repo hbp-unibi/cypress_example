f7a866d86c768b99ffd138274b705fd17def4be6
Christoph Ostrau <costrau@techfak.uni-bielefeld.de>
2019-12-06 15:31:50 +0100

    Use Python3

--- cmake/cypress_external.cmake
+++ cmake/cypress_external.cmake
@@ -15,8 +15,8 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-find_package(PythonLibs 2.7 REQUIRED )
-find_package(PythonInterp 2.7 REQUIRED)
+find_package(PythonLibs 3 REQUIRED )
+find_package(PythonInterp 3 REQUIRED)
 
 include(ExternalProject)
 
@@ -27,7 +27,7 @@ if(UPDATE_CYPRESS)
     ExternalProject_Add(cypress_ext
         GIT_REPOSITORY        "https://github.com/hbp-unibi/cypress/"
         GIT_TAG               master
-        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False
+        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False -DPYTHON_VERSION_MAJOR=${PYTHON_VERSION_MAJOR}
         INSTALL_COMMAND 	  ""
         UPDATE_COMMAND git pull
         EXCLUDE_FROM_ALL      TRUE
@@ -37,7 +37,7 @@ else()
     ExternalProject_Add(cypress_ext
         GIT_REPOSITORY        "https://github.com/hbp-unibi/cypress/"
         GIT_TAG               master
-        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False
+        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False -DPYTHON_VERSION_MAJOR=${PYTHON_VERSION_MAJOR}
         INSTALL_COMMAND 	  ""
         UPDATE_COMMAND ""
         EXCLUDE_FROM_ALL      TRUE
